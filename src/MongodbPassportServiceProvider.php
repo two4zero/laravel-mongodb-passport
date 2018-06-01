6cf209340582c7aa12c4249635ad6e63780fa995
dkseo <two4zero.g@gmail.com>
2018-06-01 14:29:18 +0900

    service provider 수정

--- src/MongodbPassportServiceProvider.php
+++ src/MongodbPassportServiceProvider.php
@@ -3,10 +3,33 @@
 namespace Two4zero\Mongodb;
 
 use Illuminate\Support\ServiceProvider;
+use Laravel\Passport\Passport;
+use Two4zero\Mongodb\Passport\AuthCode;
+use Two4zero\Mongodb\Passport\Client;
 use Two4zero\Mongodb\Passport\ClientRepository;
+use Two4zero\Mongodb\Passport\PersonalAccessClient;
+use Two4zero\Mongodb\Passport\Token;
 
 class MongodbPassportServiceProvider extends ServiceProvider
 {
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Passport::useAuthCodeModel(AuthCode::class);
+        Passport::useClientModel(Client::class);
+        Passport::usePersonalAccessClientModel(PersonalAccessClient::class);
+        Passport::useTokenModel(Token::class);
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
     public function register()
     {
         if (class_exists('Illuminate\Foundation\AliasLoader')) {
